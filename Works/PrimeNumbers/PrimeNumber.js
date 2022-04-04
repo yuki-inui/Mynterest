5c67c9c20d978616195fa2794289eb5574692cf8
yuki-inui <courage8210@gmail.com>
2022-04-04 23:08:01 +0900

    PrimerNumber

---
+++ Works/PrimeNumbers/PrimeNumber.js
@@ -0,0 +1,16 @@
+const PrimeNumber = (num) => {
+  if(num === 2) {
+    console.log(2);
+  }else{
+    for(let i = 2; i < N; i++){
+      if(N % i === 0) {
+        break;
+      }else {
+        console.log(i);
+      }
+    }
+  }
+  
+};
+
+let N = 191
