d6f50b9588121fdfa7c4f1912930285a1ab544d7
yuki-inui <courage8210@gmail.com>
2022-04-18 23:24:18 +0900

    latest

--- Works/Numbers/FateNumber/FateNumber.js
+++ Works/Numbers/FateNumber/FateNumber.js
@@ -1,9 +1,16 @@
+// 格桁の数字を足す
 const AddEveryParts = (num) => {
-    let sum = 0
-    while(num > 1){
+    let sum = 0;
+    while(num > 0){
       sum += num % 10;
       num = Math.floor(num / 10);
     }
     return sum;
   };
 
+// もし格桁の合計１０より大きかったら別の処理する
+if(AddEveryParts >= 10){
+  console.log("Over10");
+}
+
+console.log(AddEveryParts(1235));
