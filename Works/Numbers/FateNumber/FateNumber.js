29bd2e8c20632b3ebb20650103d61524472770b7
yuki-inui <courage8210@gmail.com>
2022-06-22 20:43:49 +0900

    fatenum

--- Works/Numbers/FateNumber/FateNumber.js
+++ Works/Numbers/FateNumber/FateNumber.js
@@ -12,3 +12,19 @@ const AddEveryParts = (num) => {
 // もし格桁の合計１０より大きかったら別の処理する
 
 console.log(AddEveryParts(234456));
+
+{
+for(let i = 1; i < 101; i++){
+  if(i % 3 === 0){
+    console.log("fizz");
+  } else if (i % 5 === 0){
+    console.log("buzz");
+  } else if (i % 15 === 0){
+    console.log("fizzbuzz");
+  } else{
+    console.log(i);
+  }
+}
+}
+
+console.log(33);
